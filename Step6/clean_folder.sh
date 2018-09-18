f8900e8b1396d08846e7b8e2977d8190b5145143
Haroon Rafique <haroon.rafique@cern.ch>
2018-09-18 18:44:17 +0200

    Added and completed Step6

---
+++ Step6/clean_folder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ptc cleanup
+rm flat.dat
+rm junk.txt
+rm Maxwellian_bend_for_ptc.txt
+rm SIS_18_BENCHMARK.flt
+rm ptc_twiss
+rm ptc_twiss_matched.tfs
+rm twiss_sis18_matched.tfs
+rm ptc_twiss_unmatched.tfs
+rm Negative_node.OUT
+# PyORBIT cleanup
+rm simulation_parameters.pyc
+rm -r input
+rm -r lost
+rm output/mainbunch*
+rm output.mat
+rm lib/*.pyc
